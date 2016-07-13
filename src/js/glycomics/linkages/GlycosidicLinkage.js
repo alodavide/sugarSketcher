49b361448a0320a772cef2e25b2f3726d84f9334
dalocci <davide.alocci@isb-sib.ch>
2016-07-13 15:22:19 +0200

    Add test for GlycosidicLinkage

--- src/js/glycomics/linkages/GlycosidicLinkage.js
+++ src/js/glycomics/linkages/GlycosidicLinkage.js
@@ -7,8 +7,18 @@ import Edge from '../../dataStructure/Edge';
 export default class GlycosidicLinkage extends Edge{
     constructor(id,target,source,anomerCarbon,linkedCarbon){
         super(id,target,source);
-        this.anomerCarbon = anomerCarbon;
-        this.linkedCarbon = linkedCarbon;
+
+        if(typeof anomerCarbon === 'number' && anomerCarbon> 0 && anomerCarbon < 7){
+            this.anomerCarbon = anomerCarbon;
+        } else {
+            throw "The Anomer Carbon must a numeric variable between 1 and 6";
+        }
+
+        if(typeof linkedCarbon === 'number' && linkedCarbon> 0 && linkedCarbon < 7){
+            this.linkedCarbon = linkedCarbon;
+        } else {
+            throw "The Linked Carbon must a numeric variable between 1 and 6";
+        }
     }
 
     getAnomerCarbon(){
--- src/test/glycomics/linkages/GlycosidicLinkageTest.js
+++ src/test/glycomics/linkages/GlycosidicLinkageTest.js
@@ -8,10 +8,29 @@ import GlycosidicLinkages from "../../../js/glycomics/linkages/GlycosidicLinkage
 QUnit.module("Test GlycosidicLinkage object", {
 });
 
-QUnit.test( "Getting basic paths" , function( assert ) {
+QUnit.test( "Create new glycosidic linakge" , function( assert ) {
 
     var edge = new GlycosidicLinkages('test','source1','target1',1,2);
-
     assert.ok(edge.getAnomerCarbon() === 1,"Correct residue is returned");
     assert.ok(edge.getLinkedCarbon() === 2,"Correct number of paths are returned");
 });
+
+QUnit.test( "Create new glycosidic linkage Error Linakge" , function(assert) {
+    assert.raises(function(){
+        new GlycosidicLinkages('test','source1','target1',8,1);
+    });
+
+    assert.raises(function(){
+        new GlycosidicLinkages('test','source1','target1',1,7);
+    });
+
+    assert.raises(function(){
+        new GlycosidicLinkages('test','source1','target1','error',1);
+    });
+
+    assert.raises(function(){
+        new GlycosidicLinkages('test','source1','target1',1,'error');
+    });
+});
+
+
