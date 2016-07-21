f9f290987109ec70b7ca9272d3071775ae01800f
davide alocci <davide.alocci@isb-sib.ch>
2016-07-21 12:32:41 +0200

    add webpack config for test and prod

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,9 @@ gulp.task('webpack', function() {
         .pipe(webpack(require('./webpack.config.js') ))
         .pipe(gulp.dest('dest'));
 });
+
+gulp.task('webpack-prod', function() {
+    return gulp.src('src/index.js')
+        .pipe(webpack(require('./webpack-prod.config.js') ))
+        .pipe(gulp.dest('dest'));
+});
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// webpack.config.js
+var webpack = require('webpack');
+var libraryName = 'sb';
+var outputFile = libraryName + '.js';
+
+plugins = [];
+var config = {
+    entry: __dirname + '/src/index.js',
+    devtool: 'source-map',
+    output: {
+        path: __dirname + '/dest',
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /(\.jsx|\.js)$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules|bower_component|coverage)/,
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
